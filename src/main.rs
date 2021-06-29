ff97c4e588ab60a956d2519e853b6e8ae8d00407
eribertt_fedorabox <erimendz@gmail.com>
2021-06-29 16:57:31 +0300

    add regex code lines to main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
+use regex::Regex;
+
 mod authentication;
 
 fn main() {
 	let mut user = authentication::User::new("jeremy", "super-secret");
 	println!("The username is: {}", user.get_username());
 	user.set_password("even-more-secret");
+
+	// for regex
+	let re = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
+	println!("Did our date match? {}", re.is_match("2021-06-29"));
 }
